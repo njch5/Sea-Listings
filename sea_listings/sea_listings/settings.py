80cd81e1ef7eb27ff6bcb24a9e2f08899bd23d1d
Nicky Choi <nickyjinchoi@gmail.com>
2020-01-04 22:10:48 -0800

    Installed pylint and added template directory to settings

--- sea_listings/sea_listings/settings.py
+++ sea_listings/sea_listings/settings.py
@@ -14,6 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# Below line lets Django know where our templates are
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'my_app',
 ]
 
 MIDDLEWARE = [
