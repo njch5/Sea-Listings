5d47c1efd9928da56fa3d4e684359a08799c9841
Nicky Choi <nickyjinchoi@gmail.com>
2020-01-04 22:37:05 -0800

    Created Search model and incorporated migrations

--- sea_listings/my_app/models.py
+++ sea_listings/my_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+# Created a model called Search with a field called search
+class Search(models.Model):
+  search = models.CharField(max_length=500)
+  created = models.DateTimeField(auto_now=True)
+
+  # Adds an e with search so it's properly plural-ed
+  # Column in models/databases
+  class Meta:
+    verbose_name_plural = 'Searches'
