aea10696695010ce5236bab0eb4417e1154a0682
Nicky Choi <nickyjinchoi@gmail.com>
2020-01-04 22:55:49 -0800

    Created static folder in project root directory to hold stylesheets

--- sea_listings/my_app/urls.py
+++ sea_listings/my_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+  # Run the home function from views.py file
   path('', views.home, name='home'),
   # path('admin/', admin.site.urls)
 ]
