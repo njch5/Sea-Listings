d8663d35d20269ad6daf6985a2debce8a21963f0
Nicky Choi <nickyjinchoi@gmail.com>
2020-01-04 22:45:44 -0800

    Added home view in both my_app and sea_listings urls.py files

--- sea_listings/my_app/models.py
+++ sea_listings/my_app/models.py
@@ -7,6 +7,10 @@ class Search(models.Model):
   search = models.CharField(max_length=500)
   created = models.DateTimeField(auto_now=True)
 
+  # Return the search field in the Search model in order to identify object
+  def __str__(self):
+    return '{}'.format(self.search)
+
   # Adds an e with search so it's properly plural-ed
   # Column in models/databases
   class Meta:
