9804ecc0c41c2f12102284c3d9a81d283dfad998
Nicky Choi <nickyjinchoi@gmail.com>
2020-01-07 22:00:53 -0800

    Added another test for new_search URL

--- sea_listings/my_app/tests.py
+++ sea_listings/my_app/tests.py
@@ -9,3 +9,7 @@ class TestUrls(SimpleTestCase):
   def test_home_resolves(self):
     url = reverse('home')
     self.assertEquals(resolve(url).func, home)
+
+  def test_new_search_resolves(self):
+    url = reverse('new_search')
+    self.assertEquals(resolve(url).func, new_search)
