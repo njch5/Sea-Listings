6e6d995aedccee27bea74e68a9d26f4b301f65a2
Nicky Choi <nickyjinchoi@gmail.com>
2020-01-07 21:53:03 -0800

    Wrote test for home URL

--- sea_listings/my_app/tests.py
+++ sea_listings/my_app/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+# from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from my_app.views import home, new_search
 
 # Create your tests here.
+class TestUrls(SimpleTestCase):
+
+  def test_home_resolves(self):
+    url = reverse('home')
+    self.assertEquals(resolve(url).func, home)
