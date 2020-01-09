eaaa158eab389d231832489e5e81d0b1eb6dde87
Nicky Choi <nickyjinchoi@gmail.com>
2020-01-09 15:25:39 -0800

    Now grabbing the image URLs from listings

--- sea_listings/my_app/views.py
+++ sea_listings/my_app/views.py
@@ -6,6 +6,7 @@ from bs4 import BeautifulSoup
 from . import models
 
 BASE_URL = 'https://seattle.craigslist.org/search/?query={}'
+BASE_IMAGE_URL = 'https://images.craigslist.org/{}_300x300.jpg'
 
 # Create your views here.
 def home(request):
@@ -36,8 +37,14 @@ def new_search(request):
       post_price = post.find(class_='result-price').text
     else:
       post_price = 'N/A'
-
+    
+    if post.find(class_='result-image').get('data-ids'):
+      post_image_url = post.find(class_='result-image').get('data-ids').split(',')[0].split(':')[1]
+      post_image_url = BASE_IMAGE_URL.format(post_image_url)
+      print(post_image_url)
     final_postings.append((post_title, post_url, post_price))
+
+
 
   stuff_for_frontend = {
     'search': search,
