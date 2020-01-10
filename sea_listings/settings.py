841da1abedaeec85996b36141a5c5ea1246683f0
Nicky Choi <nickyjinchoi@gmail.com>
2020-01-10 11:14:51 -0800

    Test

--- sea_listings/settings.py
+++ sea_listings/settings.py
@@ -11,9 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -30,7 +27,7 @@ SECRET_KEY = 'ar+5%%j=@%oc6yhsaoa%9kqk&yly6d%+#-=)j1qrwrkh)csnuo'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['sea-listings.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
