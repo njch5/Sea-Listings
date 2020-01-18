b6f95670758f5dea8ae4b8f997da798ebf0ae05e
Nicky Choi <nickyjinchoi@gmail.com>
2020-01-17 20:24:01 -0800

    Added validations on form.

--- my_app/tests.py
+++ my_app/tests.py
@@ -2,6 +2,7 @@
 from django.test import SimpleTestCase, TestCase, Client
 from django.urls import reverse, resolve
 from my_app.views import home, new_search
+from my_app.models import Search
 
 # Create your tests here.
 
@@ -24,14 +25,32 @@ class TestViews(TestCase):
   def setUp(self):
     self.client = Client()
     self.home_url = reverse('home')
-    self.new_search_url = reverse('new_search')
+    self.new_search = reverse('new_search')
   
   def test_home_GET(self):
     response = self.client.get(self.home_url)
     self.assertEquals(response.status_code, 200)
     self.assertTemplateUsed(response, 'base.html')
 
-  def test_new_search_GET(self):
-    response = self.client.get(self.new_search_url)
+  def test_new_search_POST(self):
+    search ='lawn chair'
+
+    response = self.client.post(self.new_search, {
+      'search': search
+    })
+
     self.assertEquals(response.status_code, 200)
-    self.assertTemplateUsed(response, 'new_search.html')
+
+
+# Model Tests
+
+class SearchModelTest(TestCase):
+  @classmethod
+  def setUpTestData(cls):
+    Search.objects.create(search='keyboard')
+  
+  def test_search_label(self):
+    search = Search.objects.get(id=1)
+    field_label = search._meta.get_field('search').verbose_name
+    self.assertEquals(field_label, 'search')
+  
